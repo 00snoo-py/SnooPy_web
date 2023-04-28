b8fadbd453fd2343bc843546c12653ff8b3c9bb1
dya-only <dyacode@proton.me>
2023-04-28 09:50:24 +0900

    add members

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import style from './App.module.css'
 import Nav from './components/Nav/nav'
 
 import SnooPy from '/imgs/snoo-py.png'
-import GBSW from '/imgs/symbol-only.png'
-import Arrow from '/imgs/arrow.png'
+import GBSW from '/imgs/symbol-white.png'
+import Arrow from '/imgs/white-arrow.png'
 
 function App() {
 
@@ -28,32 +28,72 @@ function App() {
         </div>
       </div>
 
-      {/* <div className={style.members}>
-        <div className={style.sub_title}>Members</div>
+      <div className={style.sub_title}>Members</div>
+      <div className={style.contain_contain}>
+        <div className={style.contain}>
+          <div className={style.card}>
+            <div className={style.img_contain}>
+              <img className={style.card_img} src="https://avatars.githubusercontent.com/u/108207397?v=4" alt="" />
+              <div className={style.card_info}>팀 리더</div>
+            </div>
+            <div className="">
+              <div className={style.card_work}>Developer</div>
+              <div className={style.card_name}>김정윤</div>
+              <div className={style.card_nickname}>GUCCUDAS</div>
+            </div>
+          </div>
 
-        <div className={styls.card_contain}>
           <div className={style.card}>
-            <img className={style.card_img} src="" alt="" />
-            <div className={style.card_name}>김정윤</div>
+            <div className={style.img_contain}>
+              <img className={style.card_img} src="https://avatars.githubusercontent.com/u/51194584?v=4" alt="" />
+              <div className={style.card_info}>변예현 따까리</div>
+            </div>
+            <div className="">
+              <div className={style.card_work}>Developer</div>
+              <div className={style.card_name}>손보석</div>
+              <div className={style.card_nickname}>dya_only</div>
+            </div>
           </div>
+
           <div className={style.card}>
-            <img className={style.card_img} src="" alt="" />
-            <div className={style.card_name}>손보석</div>
+            <div className={style.img_contain}>
+              <img className={style.card_img} src="https://avatars.githubusercontent.com/u/83718994?v=4" alt="" />
+              <div className={style.card_info}>의문의 임베디드 장인</div>
+            </div>
+            <div className="">
+              <div className={style.card_work}>Secure & Embedded</div>
+              <div className={style.card_name}>김동영</div>
+              <div className={style.card_nickname}>kairos-hk</div>
+            </div>
           </div>
+
           <div className={style.card}>
-            <img className={style.card_img} src="" alt="" />
-            <div className={style.card_name}>김동영</div>
+            <div className={style.img_contain}>
+              <img className={style.card_img} src="https://avatars.githubusercontent.com/u/127202879?v=4" alt="" />
+              <div className={style.card_info}>낙하산</div>
+            </div>
+            <div className="">
+              <div className={style.card_work}>Developer</div>
+              <div className={style.card_name}>변예현</div>
+              <div className={style.card_nickname}>yehyun</div>
+            </div>
           </div>
+
           <div className={style.card}>
-            <img className={style.card_img} src="" alt="" />
-            <div className={style.card_name}>변예현</div>
+            <div className={style.img_contain}>
+              <img className={style.card_img} src="https://avatars.githubusercontent.com/u/126975038?v=4" alt="" />
+              <div className={style.card_info}>dohe</div>
+            </div>
+            <div className="">
+              <div className={style.card_work}>Designer</div>
+              <div className={style.card_name}>김도희</div>
+              <div className={style.card_nickname}>dohe</div>
+            </div>
           </div>
-          <div className={style.card}>
-            <img className={style.card_img} src="" alt="" />
-            <div className={style.card_name}>김도희</div>
-          </div>
-        </div> */}
-      {/* </div> */}
+
+        </div>
+      </div>
+      
     </>
   )
 }
